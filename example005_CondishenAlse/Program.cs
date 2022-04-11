536939bc3426ef19a33384809600314550657ab7
Iljuhen <102986980+Iljuhen@users.noreply.github.com>
2022-04-12 01:24:55 +0300

    add example005_CondishenAlse

---
+++ example005_CondishenAlse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, к нам счастье привалило, это же Машка -промокашка");
+}
+else
+{
+    Console.Write("Здрасьте,  ");
+    Console.WriteLine(username);
+}
