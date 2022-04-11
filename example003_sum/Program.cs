2399425cba9932a491531864f9209112c79cda36
Iljuhen <102986980+Iljuhen@users.noreply.github.com>
2022-04-12 00:57:03 +0300

    example003_sum_random

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Для ввода данных подходит команда int
-int numberA = 25;
-int numberB = 35;
+int numberA = new Random().Next(1,10);// 1 2 3 4 ... 9
+//Чтобывидеть рандомное число пишем команду
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 //Console.WriteLine(numberA+numberB);  или
 int result = numberA+numberB;
 Console.WriteLine(result);
