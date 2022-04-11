3e64df1701878dfc850e27282fa18ae1416d1164
Iljuhen <102986980+Iljuhen@users.noreply.github.com>
2022-04-11 23:48:34 +0300

    add example002_HelloUser

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿//Для отображения консолью текста введите команду указанную ниже
+
+Console.Write("Введите ваше имя");
+
+//Для отоброжения места для ввода текста введите команду указанную ниже
+
+string username = Console.ReadLine();
+
+Console.WriteLine("Привет");
+Console.WriteLine(username);
