2ae8667e9b4c628486beb848d0cc3c1f1c60d39a
Iljuhen <102986980+Iljuhen@users.noreply.github.com>
2022-04-11 22:30:39 +0300

    example001_HelloConsole

--- example001_HelloConsole/Program.cs
+++ example001_HelloConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿object value = Console.WriteLine("2 Hello, World!");
 // dotnet new console - создать новый проект
 // dotnet run - запустить проект.
 // через console мы обращаемся к системному терминалу
