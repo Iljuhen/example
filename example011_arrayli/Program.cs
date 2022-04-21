4ccd016849e95a6fe5462699470150ffa44ba82c
Iljuhen <102986980+Iljuhen@users.noreply.github.com>
2022-04-21 20:23:57 +0300

    add o11array fin

--- example011_arrayli/Program.cs
+++ example011_arrayli/Program.cs
@@ -11,17 +11,40 @@
 }
 void PrintArray(int[] col)
 {
-    int count = Col.Length;
+    int count = col.Length;
     int position = 0;
     while (position < count)
     {
-        Console.WriteLine(Col[position]);
+        Console.WriteLine(col[position]);
         position++;
     }
 
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];// изначально заполняется нулями
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array,444 );
+Console.WriteLine(pos);
